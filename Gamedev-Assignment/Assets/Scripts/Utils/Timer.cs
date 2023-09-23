2f77a233048379a82db5b07e3a7c3e5666945c64
Kavindu <kavindupriyanath@gmail.com>
2023-09-23 18:26:38 +0530

    Added timer

---
+++ Gamedev-Assignment/Assets/Scripts/Utils/Timer.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField]
+    private TMP_Text countdownText;
+
+    private float currentTime;
+    private float initialTime = 60f; // 5 minutes in seconds
+
+    private void Start()
+    {
+        currentTime = initialTime;
+    }
+
+    private void Update()
+    {
+        if (currentTime > 0)
+        {
+            currentTime -= Time.deltaTime;
+
+            int minutes = Mathf.FloorToInt(currentTime / 60);
+            int seconds = Mathf.FloorToInt(currentTime % 60);
+
+            string timeFormatted = string.Format("{0}:{1:00}", minutes, seconds);
+            countdownText.text = timeFormatted;
+        }
+        else
+        {
+            // Countdown timer has reached zero.
+            countdownText.text = "0:00"; // You can add any desired behavior here.
+        }
+    }
+}
